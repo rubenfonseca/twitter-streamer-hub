833bcd5ac0bf2c2549f37163fd4827be0d42f16b
Ruben Fonseca <root@cpan.org>
2010-10-07 22:00:45 +0100

    move to sitestreams

--- server.js
+++ server.js
@@ -16,30 +16,8 @@ process.addListener('uncaughtException', function(err, stack) {
   console.log('--------------------------------');
 });
 
-var chainGang = require('chain-gang');
-var TwitterClient = require('twitter_client');
-
-global.config = {}
-global.clientQueue = chainGang.create({workers:1});
-global.clientCallback = function(worker) {
-  console.log(worker.performing);
-  
-  var data = JSON.parse(worker.performing);
-
-  new TwitterClient(data.oauth_token, data.oauth_token_secret, data.twitter_id);
-
-  setTimeout(function() {
-    console.log('finished client worker');
-    worker.finish();
-  }, 1000);
-};
-global.clientQueue.addListener('error', function(name, error) {
-  console.log(error);
-  console.log(name);
-});
-
 var Bootstraper = require('bootstraper');
 new Bootstraper(upstream_host, upstream_port);
 
-// new TwitterClient("oauth token", 123);
+// TwitterClient.add_client(123);
 
